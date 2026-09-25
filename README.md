01c387368b19aa08783e63b97d8e0882ea73c7fb
navin krishna <160697786+NavinKrishna05@users.noreply.github.com>
2026-09-25 17:55:25 +0530

    Added README file

---
+++ README.md
@@ -0,0 +1,29 @@
+# Playwright SauceDemo Automation Framework
+
+## Project Overview
+
+This project is an automation framework developed using Playwright and JavaScript.
+
+### Features
+- Login Automation
+- Add To Cart Validation
+- Cart Validation
+- Checkout Automation
+- API Testing (GET & POST)
+
+### Framework Design
+- Page Object Model (POM)
+- JSON Test Data
+- beforeEach Hooks
+- baseURL Configuration
+
+### Tools Used
+- Playwright
+- JavaScript
+- Git
+- GitHub
+- VS Code
+
+### Author
+Navin Krishna
+Embedded Software Test Engineer
--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,16 +1,40 @@
-import { test, expect } from '@playwright/test';
+ import { test, expect } from '@playwright/test';
 
-test('GET Users API', async ({ request }) => {
+// test('GET Users API', async ({ request }) => {
 
-    const response = await request.get(
-        'https://reqres.in/api/users?page=2'
+//     const response = await request.get(
+//         'https://reqres.in/api/users?page=2'
+//     );
+
+//     expect(response.status()).toBe(200);
+
+//     const responseBody = await response.json();
+
+//     console.log(responseBody);
+
+//     expect(responseBody.page).toBe(2);
+// });
+
+
+
+test('Create User API', async ({ request }) => {
+
+    const response = await request.post(
+        'https://reqres.in/api/users',
+        {
+            data: {
+                name: 'Navin',
+                job: 'Automation Tester'
+            }
+        }
     );
 
-    expect(response.status()).toBe(200);
+    expect(response.status()).toBe(201);
 
     const responseBody = await response.json();
 
     console.log(responseBody);
 
-    expect(responseBody.page).toBe(2);
+    expect(responseBody.name).toBe('Navin');
+    expect(responseBody.job).toBe('Automation Tester');
 });
